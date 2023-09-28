62588da89b9f10d81b00fa4229bf63fd7ad56394
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-09-28 08:18:19 -0300

    att

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ const saltRounds = 10;
 const secret = "secret-key"
 
 const db = mysql.createConnection({
-	host: "localhost",
+	host: "",
 	user: "root",
 	password: "senha1234",
 	database: "banco",
-});
+})
 
 app.use(express.json())
 app.use(cors(
     {
         // https://user-auth-client-carlosna7.vercel.app
         // http://localhost:3000
-        origin:"http://localhost:3000",
+        origin:"https://user-auth-client-carlosna7.vercel.app",
         methods: ["POST", "GET"],
         credentials: true
     }
